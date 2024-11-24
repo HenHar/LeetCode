2fe793bde63c2d62bfb04ab5fec6378746af32c3
Hendrik Ständker <45701331+HenHar@users.noreply.github.com>
2024-11-24 20:21:49 +0200

    Time: 199 ms (9.91%) | Memory: 16.9 MB (7.21%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,21 +1,16 @@
 class Solution:
-    def getNumberOfCharactersToChange(self, windowStart : int,  windowEnd : int, character_count: dict) -> int:
-        length_substring = (windowEnd + 1) - windowStart
-        charcater_max_occurence = max(character_count.values())
-        return length_substring - charcater_max_occurence
-
     def characterReplacement(self, s: str, k: int) -> int:
         character_count = {}
         maxSubString = 0
         
-        # dynamic sliding window 
+        # dynamic sliding window -> grow to right
         windowStart = 0
         for windowEnd in range(len(s)):
             currChar = s[windowEnd]
             character_count[currChar] = 1 + character_count.get(currChar, 0)
             num_characters_to_change =  self.getNumberOfCharactersToChange(windowStart, windowEnd, character_count)
 
-            # condition not fullfied
+            # condition not fullfied -> shrink window from left
             while (num_characters_to_change > k):
                 currChar = s[windowStart]
                 character_count[currChar] -= 1
@@ -27,3 +22,8 @@ class Solution:
             maxSubString = max(maxSubString, length_substring)
 
         return maxSubString
+
+    def getNumberOfCharactersToChange(self, windowStart : int,  windowEnd : int, character_count: dict) -> int:
+        length_substring = (windowEnd + 1) - windowStart
+        max_occurence_character = max(character_count.values())
+        return length_substring - max_occurence_character
