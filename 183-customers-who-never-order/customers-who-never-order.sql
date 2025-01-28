f3215bad467772a60d74f4d2bb76e9dca1d393c9
Hendrik Ständker <45701331+HenHar@users.noreply.github.com>
2025-01-28 13:23:45 +0200

    Time: 338 ms (57.69%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,6 +1,13 @@
 -- Write your PostgreSQL query statement below
+SELECT name as "Customers"
+FROM Customers
+WHERE id NOT IN
+(SELECT customerId FROM Orders);
+
+/*
 SELECT Customers.name as "Customers"
 FROM Customers
 LEFT JOIN Orders
 ON Customers.id = Orders.customerId
 WHERE Orders.customerId IS NULL;
+*/
