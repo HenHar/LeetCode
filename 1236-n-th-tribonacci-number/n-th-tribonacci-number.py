fd9771db00c19282bb35039cf93a9e7223ab51bf
Hendrik Ständker <45701331+HenHar@users.noreply.github.com>
2025-03-17 09:12:41 +0200

    Time: 0 ms (100.00%) | Memory: 18 MB (8.36%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,11 +1,28 @@
 class Solution:
-    memo = {}
     def tribonacci(self, n: int) -> int:
+        return self.dp_iterative(n)
+        return self.dp_rec(n, {})
+
+    def dp_rec(self, n: int, memo: dict) -> int:
         if n == 0: return 0
         if n == 1: return 1
         if n == 2: return 1
 
-        if n not in self.memo:
-            self.memo[n] =  self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+        if n not in memo:
+            memo[n] =  self.dp_rec(n-1, memo) + self.dp_rec(n-2, memo) + self.dp_rec(n-3, memo)
         
-        return self.memo[n]
+        return memo[n]
+
+    def dp_iterative(self, n: int) -> int:
+        memo = {}
+
+        # base cases
+        memo[0] = 0
+        memo[1] = 1
+        memo[2] = 1
+
+        for i in range(3, n+1):
+            if i not in memo:
+                memo[i] = memo[i-1] + memo[i-2] + memo[i-3]
+
+        return memo[n]
