86950a39dd00c0a48ee240e6ca6c14133fa5b714
Hendrik Ständker <45701331+HenHar@users.noreply.github.com>
2025-05-04 21:36:31 +0300

    Time: 132 ms (60.69%) | Memory: 33.1 MB (60.19%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,7 +1,6 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-        
         dp = [[0]*(cols+1) for i in range(rows+1)]
         
         max_edge = 0
@@ -11,5 +10,4 @@ class Solution:
                     dp[r][c] = 1 + min(dp[r-1][c], dp[r-1][c-1], dp[r][c-1])
                     max_edge = max(dp[r][c], max_edge)
 
-        print(dp)
         return max_edge * max_edge
