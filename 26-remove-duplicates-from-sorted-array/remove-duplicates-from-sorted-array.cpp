5d289454bd1de7303a2a6495f5fcee19a857c04e
Hendrik Ständker <45701331+HenHar@users.noreply.github.com>
2025-09-02 10:28:46 +0400

    Time: 0 ms (100.00%) | Memory: 22.5 MB (78.32%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int unique = 0;
+        for(int i = 1; i < nums.size(); i++) {
+            if (nums[unique] != nums[i]) {
+                nums[unique + 1] = nums[i];
+                unique++;
+            }
+        }
+        return unique + 1;
+    }
+};
