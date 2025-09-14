8f64a92142930de5f8faf08fde72c0f30b9d4ba1
Hendrik Ständker <45701331+HenHar@users.noreply.github.com>
2025-09-14 23:44:27 +0400

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.66%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int len_last_word = s.length();
+        int len_last_word = 0;
         bool count = false;
-        string space = " ";
         for (int i = s.length()-1; i >= 0; i--) {
-            if (!count && s.substr(i, 1).compare(space) != 0) {
+            if (s[i] != ' ') {
                 count = true;
-                len_last_word = 0;
-            }
-            if (count && s.substr(i, 1).compare(space) == 0)
-                return len_last_word;
-            if (count)
                 len_last_word++;
+            }
+            else if (count)
+                return len_last_word;
         }
         return len_last_word;
     }
