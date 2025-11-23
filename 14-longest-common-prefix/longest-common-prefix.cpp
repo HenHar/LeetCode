093f4774d0a0aa36ccda1c498cfbcce396be0fe0
Hendrik Ständker <45701331+HenHar@users.noreply.github.com>
2025-11-23 20:09:56 +0300

    Time: 3 ms (11.74%) | Memory: 14.7 MB (6.37%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = "";
+        int prefix_end = 0;
+        string word = strs[0];
+
+        while(true) {
+            prefix = word.substr(0,prefix_end);
+            for (auto& word : strs) {
+                if (prefix_end > word.size())
+                    return prefix.substr(0, prefix_end-1);
+                if (word.substr(0,prefix_end) != prefix)
+                    return prefix.substr(0, prefix_end-1);
+            }
+            prefix_end++;
+        }
+        return prefix;
+    }
+};
