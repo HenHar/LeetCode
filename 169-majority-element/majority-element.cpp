27359ca284c0097f93fe9ea32d1c34bc79dfa570
Hendrik Ständker <45701331+HenHar@users.noreply.github.com>
2026-01-06 19:58:07 +0300

    Time: 2 ms (31.52%) | Memory: 28.2 MB (61.96%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,12 +4,13 @@ public:
         map<int, int> counter;
 
         for (auto& i : nums) {
-            if (counter.contains(i)) {
-                counter[i] += 1;
-            } else {
-                counter[i] = 1;
-            }
-            if (counter[i] >= (nums.size() / 2) + 1) return i;
+            if (counter.contains(i)) counter[i] += 1;
+            else counter[i] = 1;    
+        }
+
+        int min_size = nums.size() / 2;
+        for (auto& item : counter) {
+            if (item.second > min_size) return item.first;
         }
         return 0;
 
